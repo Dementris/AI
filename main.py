95c85578556b6c913dbab7f2474ff0b1fd385e97
Dementris <bdlap.shl@gmail.com>
2023-10-23 19:48:59 +0300

    init

---
+++ main.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+
+def generate_route(N):
+    nodes = []
+    pos = []
+    for x in range(N):
+        for y in range(N):
+            pos.append((x, y))
+            nodes.append((x, y))
+
+    edges = []
+    for x in range(N):
+        for y in range(N):
+            current_node = (x, y)
+            if x < N - 1:
+                neighbor_node = (x + 1, y)
+                if neighbor_node in nodes:
+                    edges.append((current_node, neighbor_node))
+            if y < N - 1:
+                neighbor_node = (x, y + 1)
+                if neighbor_node in nodes:
+                    edges.append((current_node, neighbor_node))
+    pos = {k: v for k, v in zip(nodes,pos)}
+    return pos, edges
+
+def remove_edges(G, edges_to_remove):
+    if edges_to_remove >= len(G.edges()):
+        print("The number of edges to remove is greater "
+              "than or equal to the number of edges in the graph")
+        return G
+    edges = list(G.edges)
+
+    minimum_edges = nx.minimum_spanning_tree(G).edges
+
+    non_minimum_edges = [e for e in edges if e not in minimum_edges]
+
+    if edges_to_remove < len(minimum_edges):
+        try:
+            remove = random.sample(non_minimum_edges, edges_to_remove)
+            G.remove_edges_from(remove)
+        except(ValueError):
+            print("Too many edges to remove")
+    return G
+
+if __name__ == '__main__':
+    N = 5
+    pos, edges = generate_route(N)
+    G = nx.Graph()
+    G.add_nodes_from(pos.keys())
+    G.add_edges_from(edges)
+    nx.draw(G,pos,with_labels=True)
+    plt.show()
+    nx.draw(remove_edges(G,10),pos,with_labels=True)
+    plt.show()
